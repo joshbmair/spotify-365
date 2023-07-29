82915a8e58daeb2e8a941807df021b133f3fce43
Josh Birlingmair <jcbmair@gmail.com>
2023-07-29 16:50:05 -0500

    Stringify results

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -1,10 +1,46 @@
+import { useEffect, useState } from "react";
+
 import PageLayout from "@/components/PageLayout";
+import { getAccessToken, getTopArtists, getTopTracks } from "@/lib/requests";
+import { Card } from "react-bootstrap";
 
 export default function Results(): JSX.Element {
+  const [topArtists, setTopArtists] = useState<string>(
+    "Loading top artists..."
+  );
+  const [topTracks, setTopTracks] = useState<string>("Loading top tracks...");
+
+  useEffect((): void => {
+    const urlParams: URLSearchParams = new URLSearchParams(
+      window.location.search
+    );
+    const code: string = urlParams.get("code")!;
+
+    getAccessToken(code).then((token) => {
+      getTopArtists(token).then((artists) =>
+        setTopArtists(JSON.stringify(artists))
+      );
+      getTopTracks(token).then((tracks) =>
+        setTopTracks(JSON.stringify(tracks))
+      );
+    });
+  }, []);
+
   return (
     <PageLayout siteSubtitle="Results">
-      <h1>Results</h1>
-      <p>Coming soon</p>
+      <Card>
+        <Card.Body>
+          <Card.Title>Top artists</Card.Title>
+          <Card.Text>{topArtists}</Card.Text>
+        </Card.Body>
+      </Card>
+      <br />
+      <Card>
+        <Card.Body>
+          <Card.Title>Top tracks</Card.Title>
+          <Card.Text>{topTracks}</Card.Text>
+        </Card.Body>
+      </Card>
     </PageLayout>
   );
 }
